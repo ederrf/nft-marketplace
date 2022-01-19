6fc0987def1d4fc42a00d6b4b1363fa1e0d0c68e
eferreira <eferreira@fanatics.com>
2022-01-19 12:47:47 -0300

    Final pieces

---
+++ config.js
@@ -0,0 +1,6 @@
+//export const nftaddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+//export const nftmarketaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+
+
+export const nftmarketaddress = "0xf5C9d8185A47C15600dFD233D7d9c837423747C7"
+export const nftaddress = "0x1062A5Ae5B41aF9959952b91e7d300533cA48afC"
